b03d589770058afbc38ca19f5488399222fa279e
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-08-26 19:59:56 -0400

    segunda version

--- 2-base-js/src/App.js
+++ 2-base-js/src/App.js
@@ -1,1 +1,21 @@
-alert("Hola");
+const nombre = document.querySelector("#nombre-input");
+const edad = document.querySelector("#edad-input");
+const form = document.querySelector("#saludador-form");
+
+form.addEventListener("submit", (event) => {
+
+    // Definir text1 tomando en cuenta la edad
+    if(edad.value < 10 && edad.value > 5){
+        text1 = "niño(a)";
+    }
+
+    if(edad.value <= 24 && edad.value >= 10){
+        text1 = "joven";
+    }
+
+    if(edad.value < 100 && edad.value > 24){
+        text1 = "señor(a)";
+    }
+
+    alert("Hola " + text1 + " " + nombre.value);
+});
