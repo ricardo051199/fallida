08403cf87977f18f9bd076db67e626fe6f28325e
ricardo pelaez <jesus.pelaez@ucb.edu.bo>
2021-08-26 20:10:49 -0400

    tercera version

--- 2-base-js/src/App.js
+++ 2-base-js/src/App.js
@@ -1,5 +1,6 @@
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
+const genero = document.querySelector("#genero-input");
 const form = document.querySelector("#saludador-form");
 
 form.addEventListener("submit", (event) => {
@@ -17,5 +18,33 @@ form.addEventListener("submit", (event) => {
         text1 = "señor(a)";
     }
 
-    alert("Hola " + text1 + " " + nombre.value);
+    // Definir text2 tomando en cuenta el genero
+    if(text1 == "niño(a)"){
+        if(genero.value == "Hombre"){
+            text2 = "niño";
+        }
+        if(genero.value == "Mujer"){
+            text2 = "niña";
+        }
+    }
+
+    if(text1 == "joven"){
+        if(genero.value == "Hombre"){
+            text2 = "joven";
+        }
+        if(genero.value == "Mujer"){
+            text2 = "señorita";
+        }
+    }
+
+    if(text1 == "señor(a)"){
+        if(genero.value == "Hombre"){
+            text2 = "señor";
+        }
+        if(genero.value == "Mujer"){
+            text2 = "señara";
+        }
+    }
+
+    alert("Hola " + text2 + " " + nombre.value);
 });
